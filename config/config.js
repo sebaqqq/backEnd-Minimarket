c10ce8499605dc4c9045c31d7483644695af8bee
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2024-07-20 00:37:11 -0400

    test

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 console.log(process.env.DB_HOST);
 
+
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -10,4 +12,22 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql",
     logging: process.env.DB_LOGGING === "true" ? console.log : false,
   },
+
+  test:{
+    username:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    host:process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    logging: process.env.DB_LOGGING === "true" ? console.log : false,
+  },
+  production:{
+    username:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    host:process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    logging: process.env.DB_LOGGING === "true" ? console.log : false,
+  }
 };
+
